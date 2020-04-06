f43b759cd4e379cbc39fc38163c106cb5676a050
Noah Glaser <glaserpower@gmail.com>
2020-04-05 22:44:43 -0700

    about page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,13 +1,14 @@
 <svelte:head>
   <title>About Covid 19</title>
 </svelte:head>
-
-<div class="container">
-  <h1>About</h1>
-  <p>
-    The data for this site came from
-    <a href="http://covidtracking.com">Covid Tracking.com</a>
-    . You can can learn more about where the data is coming from. There are a
-    lot of sources and caveats.
-  </p>
+<div class="section header">
+  <div class="container">
+    <h1>About</h1>
+    <p>
+      The data for this site came from
+      <a href="http://covidtracking.com">Covid Tracking.com</a>
+      . You can can learn more about where the data is coming from. There are a
+      lot of sources and caveats.
+    </p>
+  </div>
 </div>
